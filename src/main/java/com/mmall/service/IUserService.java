df9a1fa5ddbb0c57208aecfa2531e2547630a673
foreverzxq <627843217@qq.com>
2017-11-01 22:40:13 +0800

    20171101

--- src/main/java/com/mmall/service/IUserService.java
+++ src/main/java/com/mmall/service/IUserService.java
@@ -1,8 +1,12 @@
 package com.mmall.service;
 
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+
 /**
  * Created by  Dongdj.
  */
 public interface IUserService {
+  ServerResponse<User> login(String username,String password);
 
 }
--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -1,9 +1,24 @@
 package com.mmall.service.impl;
 
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.UserMapper;
+import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by  Dongdj.
  */
 public class UserServiceImpl  implements IUserService{
+
+    @Autowired
+    private UserMapper userMapper;
+    @Override
+    public ServerResponse<User> login(String username, String password) {
+         int resultCount = userMapper.checkUsername(username);
+         if(resultCount==0){
+             return ServerResponse.createByErrorMessage("用户名不存在");
+         }
+        return null;
+    }
 }
